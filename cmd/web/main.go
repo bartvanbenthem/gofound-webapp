24e72fd77a90dd00ef52e5db591f99604d4fbad1
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-07-30 17:30:44 +0200

    update

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/bartvanbenthem/gofound-web/pkg/config"
 	"github.com/bartvanbenthem/gofound-web/pkg/handlers"
+	"github.com/bartvanbenthem/gofound-web/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
