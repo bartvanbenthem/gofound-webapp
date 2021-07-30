a810b5f691c1a01c9e9ed67d5ddd8abc3e5141df
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-07-30 15:36:53 +0200

    re-organize pkg

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/bartvanbenthem/gofound-web/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main is the main function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,17 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/bartvanbenthem/gofound-web/pkg/render"
 )
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
