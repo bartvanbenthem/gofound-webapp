97a77ab1e81f1296a4c1bc1e558da56b905b5c3b
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-07-31 11:48:21 +0200

    update pkg

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/bartvanbenthem/gofound-web/pkg/config"
+	"github.com/bartvanbenthem/gofound-web/pkg/models"
 	"github.com/bartvanbenthem/gofound-web/pkg/render"
 )
 
@@ -29,10 +30,17 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"github.com/bartvanbenthem/gofound-web/pkg/config"
+	"github.com/bartvanbenthem/gofound-web/pkg/models"
 )
 
 var functions = template.FuncMap{}
@@ -20,8 +21,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -38,7 +44,9 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	buf := new(bytes.Buffer)
 
-	_ = t.Execute(buf, nil)
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 	if err != nil {
