6cd6a47afd9ba94b7496af81f46b73d5cb54f6f1
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-07-31 16:03:06 +0200

    update middleware

---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bartvanbenthem/gofound-web/pkg/config"
+	"github.com/bartvanbenthem/gofound-web/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/bartvanbenthem/gofound-web
 
 go 1.15
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/justinas/nosurf v1.1.1
+)
