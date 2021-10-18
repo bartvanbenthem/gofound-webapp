0d89ed9e84b4a95b681586266e93972085f3ffc4
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-10-18 14:16:42 +0200

    update handlers tests

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+type postData struct {
+	key   string
+	value string
+}
+
+var theTests = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
+	expectedStatusCode int
+}{
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"items", "/items", "GET", []postData{}, http.StatusOK},
+	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"testform", "/testform", "GET", []postData{}, http.StatusOK},
+	{"posttestform", "/testform", "POST", []postData{
+		{key: "name", value: "John"},
+		{key: "email", value: "John@gofound.nl"},
+	}, http.StatusOK},
+}
+
+func TestHandlers(t *testing.T) {
+	routes := getRoutes()
+
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTests {
+		if e.method == "GET" {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		}
+	}
+}
---
+++ internal/handlers/setup_tests.go
@@ -0,0 +1,129 @@
+package handlers
+
+import (
+	"encoding/gob"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/bartvanbenthem/gofound-webapp/internal/config"
+	"github.com/bartvanbenthem/gofound-webapp/internal/models"
+	"github.com/bartvanbenthem/gofound-webapp/internal/render"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/justinas/nosurf"
+)
+
+var app config.AppConfig
+var session *scs.SessionManager
+var pathToTemplates = "./../../templates"
+var functions = template.FuncMap{}
+
+func getRoutes() http.Handler {
+	// declare what the session can consist of
+	gob.Register(models.TestForm{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Recoverer)
+	//router.Use(NoSurf)
+	router.Use(SessionLoad)
+
+	router.Get("/status", Repo.StatusHandler)
+
+	router.Get("/", Repo.Home)
+	router.Get("/home", Repo.Home)
+	router.Get("/about", Repo.About)
+	router.Get("/items", Repo.Items)
+	router.Get("/contact", Repo.Contact)
+	router.Get("/testform", Repo.TestForm)
+	router.Post("/testform", Repo.PostTestForm)
+	router.Get("/testform-response", Repo.TestFormResponse)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	router.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return router
+}
+
+// NoSurf is the csrf protection middleware
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+// SessionLoad loads and saves session data for current request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
+
+// CreateTestTemplateCache creates a template cache as a map
+func CreateTestTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
