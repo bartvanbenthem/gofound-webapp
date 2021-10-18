3264a808bdea5e7ceb8a3eb4ceb4e1ddf383e516
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-10-18 17:03:30 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# go test -v
+# go test -cover
+# go test -coverprofile=coverage.out && go tool cover -html=coverage.out
+
 go build -o build/bin/webserver $(ls -1 cmd/web/*.go | grep -v _test.go)
 ./build/bin/webserver
