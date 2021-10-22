6465aa10d1b5ceaca61eea77eecf23bc84dcbd3c
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-10-22 10:48:18 +0200

    update test coverage

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 # go test -v -cover internal/render/*
 # go test -v -cover internal/forms/*
 
-# go test -coverprofile=coverage.out && go tool cover -html=coverage.out
+# go test ./... -coverprofile=coverage.out && go tool cover -html=coverage.out
 
 go build -o build/bin/webserver $(ls -1 cmd/web/*.go | grep -v _test.go)
 ./build/bin/webserver
