8d3f61579f83aa35b4224fb8394d7896d6c00f2a
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-08-31 20:49:50 +0200

    update dsn

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # go run /usr/local/go/src/crypto/tls/generate_cert.go --rsa-bits=2048 --host=localhost
 
 go test -vet=off -v ./cmd/web/
-go run ./cmd/web/ --addr=":4000" # >>/tmp/info.log 2>>/tmp/error.log
+go run ./cmd/web/ --addr=":4000" --dsn="web:pass@/snippetbox?parseTime=true" # >>/tmp/info.log 2>>/tmp/error.log
